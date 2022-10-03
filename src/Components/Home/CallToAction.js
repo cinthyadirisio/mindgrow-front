0ef91e2fafbba68705742a92118c338c9c724b3a
Natalia Torres <nataliatorres88@gmail.com>
2022-10-03 16:22:56 -0500

    created call to action

---
+++ src/Components/Home/CallToAction.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../Styles/CallToAction.css'
+import {Link as LinkRouter} from 'react-router-dom'
+
+
+export default function CallToAction(props) {
+
+    let linkTo = props.linkTo
+    let buttonAction = props.buttonAction
+
+
+  return (
+
+    <div>
+        <LinkRouter to={linkTo} className='Action-Button'> {buttonAction}</LinkRouter>
+    </div>
+
+  )
+}
