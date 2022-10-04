5f15caca1bcbce4c8cd3512c777c0efd0893fb87
Natalia Torres <nataliatorres88@gmail.com>
2022-10-04 13:54:52 -0500

    updted userAPI

---
+++ src/features/userAPI.js
@@ -0,0 +1,61 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import apiurl from "../api";
+export const usersAPI = createApi({
+    reducerPath: 'usersAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${apiurl}`
+    }),
+    tagTypes: ['Post'],
+    endpoints: (builder) => ({
+        signUp: builder.mutation({
+            query: (body) => ({
+                url: '/users/signup',
+                method: 'POST',
+                body: body,
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            }),
+            invalidatesTags: ['Post'],
+        }),
+        signIn: builder.mutation({
+            query: (body) => ({
+                url: '/users/signin',
+                method: 'POST',
+                body: body,
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            }),
+            invalidatesTags: ['Post'],
+        }),
+        signInToken: builder.mutation({
+            query: (token) => ({
+                url: '/users/token',
+                method: 'GET',
+                headers: {Authorization: `Bearer ${token}` }
+            })
+        }),
+        signOut: builder.mutation({
+            query: (body) => ({
+                url: '/users/signout',
+                method: 'POST',
+                body: body,
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            }),
+            invalidatesTags: ['Post'],
+        }),
+        editProfile: builder.mutation({
+            query: (body) => ({
+                url: '/users/editProfile',
+                method: 'PATCH',
+                body: body,
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            }),
+        }),
+    })
+})
+export default usersAPI
+export const { useSignUpMutation, useSignInMutation, useSignOutMutation, useSignInTokenMutation, useEditProfileMutation } = usersAPI
