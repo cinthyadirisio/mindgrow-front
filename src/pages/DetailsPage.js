9740419632861fe400d8d83271df25c00ab7e035
emazou <estefania.mazou@gmail.com>
2022-10-04 18:15:54 -0500

    Details component added

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Details from '../components/products/Details'
+import '../styles/Details.css'
+export default function DetailsPage() {
+  return (
+    <div className='details-container'>
+        <Details />
+    </div>
+  )
+}
