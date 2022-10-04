d999b186ce726ce423245ade5a4dacee25fa0c74
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-10-04 14:55:25 -0300

    carousel + fix header

--- src/components/home/ProductCarousel.js
+++ src/components/home/ProductCarousel.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Carousel } from 'antd';
+import React from 'react';
 
-export default function ProductCarousel() {
-  return (
-    <div>ProductCarousel</div>
-  )
-}
+
+
+const ProductCarousel = () => (
+
+  <Carousel autoplay className='carousel'>
+    <div className='carousel-div'>
+      <img src="https://weedmaps.com/learn/wp-content/uploads/2020/05/181210_Nugs_017.jpg" alt="flowers"/>
+        <h3>Flowers</h3>
+    </div>
+    <div className='carousel-div'>
+      <img src="https://fh1w93s8iw-flywheel.netdna-ssl.com/wp-content/uploads/2016/02/cannabistopical.jpg" alt="topicals"/>
+        <h3>Topicals</h3>
+    </div>
+    <div className='carousel-div'>
+      <img src="https://static.wikileaf.com/uploads/2020/03/vaporizer-down-picture-id861451686.jpg" alt="Vapers"/>
+        <h3>Vapers</h3>
+    </div>
+    <div className='carousel-div'>
+      <img src="https://www.nm.org//-/media/northwestern/healthbeat/images/medical-advances/science-research/nm-cbd-oil_feature.jpg" alt="oils"/>
+        <h3>Oils</h3>
+    </div>
+  </Carousel>
+
+);
+
+export default ProductCarousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
+import ProductCarousel from '../components/home/ProductCarousel'
 
 export default function Home() {
   
   return (
+    <>
     <Hero/>
+    <ProductCarousel/>
+    </>
   )
 }
 
