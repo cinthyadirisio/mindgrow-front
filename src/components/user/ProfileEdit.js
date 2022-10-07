0b291f6d4d1a2cca874832c2c64e734a3da569d6
Natalia Torres <nataliatorres88@gmail.com>
2022-10-07 15:19:19 -0500

    created edit profile

---
+++ src/components/user/ProfileEdit.js
@@ -0,0 +1,92 @@
+import React, { useRef, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { useEditProfileMutation} from '../../features/userAPI'
+
+
+
+export default function ProfileEdit(props) {
+   const [name, setName] = useState(props.name)
+   const [lastName, setLastName] = useState(props.lastName)
+   const [country, setCountry] = useState(props.country)
+   const [photo, setPhoto] = useState(props.photo)
+   const [mail, setMail] = useState(props.mail)
+   
+    const { id } = useParams()
+    const [editProfile] = useEditProfileMutation()
+
+    const nameRef = useRef()
+    const lastNameRef = useRef()
+    const countryRef = useRef()
+    const photoRef = useRef()
+    const mailRef = useRef()
+
+    const profileArray = [
+        { item: "Name", name: "name", type: "text", id: "prof1", value: nameRef, defaultValue: `${name}` },
+        { item: "Last Name", name: 'lastName', type: "text", id: "prof2", value: lastNameRef, defaultValue: `${lastName}` },
+        { item: "Country", name: "country", type: "text", id: "prof3", value: countryRef, defaultValue: `${country}` },
+        { item: "Photo", name: "photo", type: "url", id: "prof4", value: photoRef, defaultValue: `${photo}` },
+        { item: "Email", name: "mail", type: "email", id: "prof5", value: mailRef, defaultValue: `${mail}` }
+    ];
+
+
+    const handleSubmitUpdateProfile = (e) => {
+        e.preventDefault()
+
+        let profileUpdated = {
+            name: nameRef.current.value,
+            lastName: lastNameRef.current.value,
+            country: countryRef.current.value,
+            photo: photoRef.current.value,
+            mail: mailRef.current.value,
+            _id: `${props.id}`,
+        }
+        editProfile(profileUpdated).then((res) => {
+            console.log(res)
+            console.log(localStorage.getItem('token'))
+            if (res.error) {
+                let dataError = res.error;
+                let dataMessage = dataError.data;
+
+            } else {
+                let dataResponse = res.data;
+                let dataSuccess = dataResponse.message;
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+
+    return (
+        <form id='form-edit-prof' onSubmit={handleSubmitUpdateProfile}>
+            {profileArray.map((e) => {
+                return (
+                    <label key={e.id} htmlFor={e.name}> {e.item}
+                        <input onChange={(i)=>{
+                            if(e.name == 'name'){
+                                return setName(i.target.value)
+
+                            }else if(e.name == 'lastName'){
+                                return setLastName(i.target.value)
+
+                            }else if(e.name == 'country'){
+                                return setCountry(i.target.value)
+
+                            }else if(e.name == 'photo'){
+                                return setPhoto(i.target.value)
+
+                            }else{
+                                return setMail(i.target.value)
+
+                            }
+                        }
+                        } type="text" name={e.name} ref={e.value} value={e.defaultValue } />
+                    </label>
+                );
+            })}
+            <button className="submit" type="submit ">Edit!</button>
+        </form>
+        
+    )
+
+}
