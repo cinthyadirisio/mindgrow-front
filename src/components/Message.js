da17438170896532936d8093dc255f91955ac753
emazou <estefania.mazou@gmail.com>
2022-10-09 17:24:24 -0500

    message created

---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Message.css'
+export default function Message() {
+    return (
+        <div className='container-message'>
+            <h2>Thanks for your purchase!!</h2>
+            <button>Continue shopping</button>
+        </div>
+    )
+}
